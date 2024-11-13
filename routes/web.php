3c74b0a4e4957b12b51fa2c2761b4d819abd980e
keniiiiiii <kenidumb112@gmail.com>
2024-11-13 10:13:32 +0800

    sample

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/menu', function () {
+    return view('menu');
+});
+
+Route::get('/help', function () {
+    return view('help');
+});
+
+Route::get('/sign-up', function () {
+    return view('sign-up');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
