d28420ea1ac1c76a4c0651f6310b29f3b99dc39f
Clarencee10 <157667853+Clarencee10@users.noreply.github.com>
2024-11-15 23:59:59 +0800

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,12 +1,43 @@
-@extends('Components.layout')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login to PICKK Recipes</title>
+    <link rel="stylesheet" href="/assets/css/dashboard.css">
+</head>
+<body>
 
-@section('additional-styles')
-<link rel="stylesheet" href="/assets/css/home.css">
-@endsection
+    <!-- HEADER -->
+        <header>
 
-@section('content')
+            <div class="logo">
+                <a href="/">
+                    <img src="/css/assets/images/logo.png" alt="Logo">
+                </a>
+            </div>
+ 
+            <nav>
+                <ul>
+                    <li><a href="{{ route('home') }}" class="button"><b>Explore</b></a></li>
+                    <li><a href="{{ route('recipe') }}" class="button"><b>Recipe</b></a></li>
+                    <li><a href="{{ route('menu') }}" class="button"><b>Menu</b></a></li>
+                    <li><a href="{{ route('help') }}" class="button"><b>Help</b></a></li>
 
-    <!--WELCOME SECTION-->
+                    <li class="dropdown">
+                        <a href="#" class="button"><b>More</b></a>
+                            <div class="dropdown-content">
+                                <a href="{{ route('profile') }}">Profile</a>
+                                <a href="{{ route('settings') }}">Settings</a>
+                                <a href="{{ route('logout') }}">Logout</a>
+                            </div>
+                    </li>
+                </ul>
+            </nav>
+
+        </header>
+
+
         <section id="welcome-section">
             <div class="welcome-container">
                 <img src="/assets/images/welcome.jpg" alt="welcome image">
@@ -17,6 +48,15 @@
                     </div>
             </div>
         </section>
-    <!--END OF WELCOME SECTION-->
 
-@endsection
+         <!-- FOOTER -->
+         <footer>
+            <p>&copy;copyright 2024 PICKK Recipe</p>
+            <div class="footer-links">
+                <a href="/terms">Terms of Use</a>
+                <a href="/policy">Privacy Policy</a>
+            </div>
+        </footer>
+    <!-- END OF FOOTER -->
+</body>
+</html>
